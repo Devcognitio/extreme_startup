861d83be24aea47adb88337478a16bf457cd8eb0
Carlos Acevedo <cacevedod@devco.com.co>
2020-11-05 17:43:06 -0500

    nombres en los run para facilitar el uso

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 3000:3000 extreme_startup
+docker run --name mymain -d -p 3000:3000 extreme_startup
--- warmup.sh
+++ warmup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 3000:3000 -e WARMUP=1 extreme_startup
+docker run --name mymain_warmup -d -p 3000:3000 -e WARMUP=1 extreme_startup
